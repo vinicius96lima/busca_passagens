6bbbd02658aecf8781cf32f90d51f442c93822f0
vinicius96lima <viniciusaraujo_96@hotmail.com>
2026-05-08 10:14:13 -0300

    Adicionando mais de um Destino

--- src/main.py
+++ src/main.py
@@ -43,60 +43,76 @@ def salvar_voo(voos):
 
     return df_final
 
-def buscar_passagens(origens, destino, data_ida, data_volta, valor_maximo, adultos=2):
+def buscar_passagens(origens, destinos, data_ida, data_volta, valor_maximo, adultos=2):
     todos_voos = []
 
     for origem in origens:
-        params = {
-            "engine": "google_flights",
-            "departure_id": origem,
-            "arrival_id": destino,
-            "outbound_date": data_ida,
-            "return_date": data_volta,
-            "currency": "BRL",
-            "hl": "pt",
-            "adults": adultos,
-            "type": "1",
-            "api_key": os.getenv("SERPAPI_KEY")
-        }
+        print(f'Iniciando Buscas com Origem, {origem}')
 
-        search = GoogleSearch(params)
-        result = search.get_dict()
-        print("Chaves retornadas:", result.keys())
-        print("Resultado completo:", result)
+        for destino in destinos:
+            print(f'Iniciando Busca com Destino, {destino}')
 
-        # atribui a váriavel os melhores valores segundo o google, com base em combinações, e caso não encontre, o código não é quebrado, pois retorna uma lista vazia.
-        melhores = result.get("best_flights", [])
-        outros = result.get("other_flights", [])
-        print(f"Melhores: {len(melhores)} | Outros: {len(outros)}")
+            params = {
+                "engine": "google_flights",
+                "departure_id": origem,
+                "arrival_id": destino,
+                "outbound_date": data_ida,
+                "return_date": data_volta,
+                "currency": "BRL",
+                "hl": "pt",
+                "adults": adultos,
+                "type": "1",
+                "api_key": os.getenv("SERPAPI_KEY")
+            }
+
+            search = GoogleSearch(params)
+            result = search.get_dict()
+
+            # atribui a váriavel os melhores valores segundo o google, com base em combinações, e caso não encontre, o código não é quebrado, pois retorna uma lista vazia.
+            melhores = result.get("best_flights", [])
+            outros = result.get("other_flights", [])
+            print(f"Melhores: {len(melhores)} | Outros: {len(outros)}")
 
         # Adiciona a origem em cada voo para saber de onde veio
-        for voo in melhores + outros:
-            preco = voo.get('price', 0)
+            for voo in melhores + outros:
+                preco = voo.get('price', 0)
+                link_geral = result.get("search_metadata", {}).get("google_flights_url")
+                booking_token = voo.get("booking_token")
+                link = None
 
+                if booking_token:
+                    link =  f"https://www.google.com/travel/flights?tfs={booking_token}"
+                else:
+                    link = link_geral or f"https://www.google.com/travel/flights?q={origem}-{destino}"
 
-            link_geral = result.get("search_metadata", {}).get("google_flights_url")
-            booking_token = voo.get("booking_token")
-            link = None
+                if preco <= 0:
+                    print('Preço inválido')
 
-            if booking_token:
-                link =  f"https://www.google.com/travel/flights?tfs={booking_token}"
-            else:
-                link = link_geral or f"https://www.google.com/travel/flights?q={origem}-{destino}"
+                elif preco <= valor_maximo:
+                    voo['origem_busca'] = origem
+                    voo['destino_busca'] = destino
+                    voo['link'] = link
+                    todos_voos.append(voo)
+                    print(f"✅ Voo encontrado! "
+                          f"{origem} → {destino} | R${preco} para {adultos} pessoas")
 
-            if preco > 0 and preco <= valor_maximo:
-                voo['origem_busca'] = origem
-                voo['link'] = link
-                todos_voos.append(voo)
-                print(f"✅ Voo encontrado! {origem} → {destino} | R${preco} para {adultos} pessoas")
-            else:
-                print(f"❌ Voo acima do limite | R${preco} > R${valor_maximo}")
+                elif preco <= valor_maximo * 1.20:
+                    voo['origem_busca'] = origem
+                    voo['destino_busca'] = destino
+                    voo['link'] = link
+                    todos_voos.append(voo)
+                    print(f"✅ Voo encontrado 20 por cento acima do valor esperado! "
+                          f"{origem} → {destino} | R${preco} para {adultos} pessoas")
 
-    # Salva no histórico
-    if todos_voos:
-        salvar_voo(todos_voos)
-    else:
-        print("Nenhum voo encontrado dentro do valor máximo")
+                else:
+                    print(f"❌ Voo acima do limite!"
+                          f"{origem} → {destino} | R${preco} > R${valor_maximo}")
+
+        # Salva no histórico
+        if todos_voos:
+            salvar_voo(todos_voos)
+        else:
+            print("Nenhum voo encontrado dentro do valor máximo")
 
     return todos_voos
 
@@ -152,7 +168,7 @@ def main():
     print(f"\n🔍 Iniciando busca: {datetime.today().strftime('%d/%m/%Y %H:%M')}")
     voos = buscar_passagens(
         origens=["GRU", "CGH", "VCP"],
-        destino="POA",
+        destinos=["POA","VIX"],
         data_ida="2026-09-04",
         data_volta="2026-09-07",
         valor_maximo=1500.00,
